10450a532f592ee270ceabf1056a6510b58aed8a
saishreeya@gmail.com <skanta@ad.unc.edu>
2023-09-09 17:46:56 -0400

    files added

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+@app.route('/submit', methods=[POST])
+def submit_form() {
+    stockId = request.form.get('stockname')
+
+    return f''
+}
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
