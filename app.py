1141a819497d89fb2f39c3a3b30b62a5ebdf3f9d
saishreeya@gmail.com <skanta@ad.unc.edu>
2023-09-09 17:58:33 -0400

    app.py file changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -6,12 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/submit', methods=[POST])
-def submit_form() {
+@app.route('/submit', methods=['POST'])
+def submit_form():
     stockId = request.form.get('stockname')
 
-    return f''
-}
+    return f'tickerID: {stockId}'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
