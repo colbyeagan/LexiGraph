a1515db98883a041a5711dfcb36c99cfa33de0ae
saishreeya@gmail.com <skanta@ad.unc.edu>
2023-09-09 21:31:35 -0400

    graph on about page

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import base64
 import matplotlib
 matplotlib.use('Agg')
 from flask import Flask, render_template, url_for, request, Response
@@ -10,13 +11,10 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-=======
 custom_font_path = 'OpenDyslexic3-Regular.ttf'
 custom_font = FontProperties(fname=custom_font_path)
 
 
->>>>>>> c864b67baf93d3225fecea75a589dc97ea8de83b
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -45,21 +43,10 @@ def create_plot_standard(tickerSymbol, df):
 def create_plot(tickerSymbol):
     end_date = datetime.strptime('2018-03-27', '%Y-%m-%d')
     start_date = end_date - timedelta(days=91)
-<<<<<<< HEAD
-    stockId = request.form.get('stockname')
-    url = f"https://data.nasdaq.com/api/v3/datasets/WIKI/{stockId.upper()}.csv?collapse=none&start_date={start_date}&end_date={end_date}&api_key=tMexE-dhnFFSApsTQVgz"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        font_sizing = 15
-        label_sizing = 30
-
-=======
     url = f"https://data.nasdaq.com/api/v3/datasets/WIKI/{tickerSymbol.upper()}.csv?collapse=none&start_date={start_date}&end_date={end_date}&api_key=tMexE-dhnFFSApsTQVgz"
     response = requests.get(url)
 
     if response.status_code == 200:
->>>>>>> c864b67baf93d3225fecea75a589dc97ea8de83b
         # Use Pandas to read the CSV data from the response and create a DataFrame
         df = pd.read_csv(StringIO(response.text))
         # Convert the 'Date' column to a datetime data type
@@ -74,98 +61,14 @@ def create_plot(tickerSymbol):
         img_buffer = BytesIO()
         plt.savefig(img_buffer, format='png')
         img_buffer.seek(0)
+
+        image_data_base64 = base64.b64encode(img_buffer.read()).decode('utf-8')
+
         # Display the chart
-        return render_template('about.html', image_data=img_buffer.read().encode('base64'))
+        return render_template('about.html', image_data=image_data_base64)
     else:
         return 'API request failed with status code:', response.status_code
 
-<<<<<<< HEAD
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
-custom_font_path = 'OpenDyslexic3-Regular.ttf'
-custom_font = FontProperties(fname=custom_font_path)
-
-dyslexia_friendly_settings = {
-    'font.family': 'Arial',
-    'font.size': 14,
-    'font.style': 'normal',  
-    'axes.labelsize': 12,    
-    'axes.titlesize': 16,    
-    'axes.titleweight': 'bold',
-    'axes.titlepad': 20,     
-    'axes.labelweight': 'bold',
-    'xtick.labelsize': 12,   
-    'ytick.labelsize': 12,   
-    'legend.fontsize': 12,   
-    'lines.linewidth': 2,    
-    'lines.markersize': 8,   
-    'legend.title_fontsize': 12,
-    'axes.labelcolor': 'black',  
-    'text.color': 'black',  
-    'axes.edgecolor': 'black',  
-    'axes.facecolor': 'white',  
-    'axes.grid': True,      
-    'axes.grid.axis': 'both',  
-    'grid.color': 'gray',   
-    'grid.linestyle': '--',  
-    'grid.linewidth': 0.5   
-}
-
-
-
-
-"""tickerName = stockId
-
-    end_date = datetime.strptime('2018-03-27', '%Y-%m-%d')
-    start_date = end_date - timedelta(days=91)
-    url = f"https://data.nasdaq.com/api/v3/datasets/WIKI/{tickerName}.csv?collapse=none&start_date={start_date}&end_date={end_date}&api_key=tMexE-dhnFFSApsTQVgz"
-
-    response = requests.get(url)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        font_sizing = 15
-        label_sizing = 30
-
-        # Use Pandas to read the CSV data from the response and create a DataFrame
-        df = pd.read_csv(StringIO(response.text))
-        
-        # Convert the 'Date' column to a datetime data type
-        df['Date'] = pd.to_datetime(df['Date'])
-        
-        # Set the 'Date' column as the index
-        df.set_index('Date', inplace=True)
-        
-        # Create a time series line chart
-        plt.rcParams.update(dyslexia_friendly_settings)
-        plt.figure(figsize=(12, 6))  # Adjust the figure size as needed
-        plt.plot(df.index, df['Open'], linestyle='-', color='b', label='Price')
-        plt.xlabel('Date', labelpad=20, fontproperties=custom_font, fontsize=label_sizing)
-        plt.ylabel('Value', labelpad=20, fontproperties=custom_font, fontsize=label_sizing)
-        plt.title(f'{tickerName} Chart', fontproperties=custom_font, fontsize=label_sizing)
-        plt.grid(True)
-        plt.legend([(tickerName)], prop=custom_font, fontsize=font_sizing)
-        plt.tight_layout()
-        plt.xticks(fontproperties=custom_font, fontsize=font_sizing)
-        plt.yticks(fontproperties=custom_font, fontsize=font_sizing)
-        
-        # Display the chart
-        plt.show()
-    else:
-        print("API request failed with status code:", response.status_code)
-"""
-
-    ####################################### CHARTING ###################################
-
-
-
-
-
-=======
 #route to fetch time series analysis graph
 @app.route('/submit', methods=['POST'])
 def fetch_data():
@@ -173,11 +76,5 @@ def fetch_data():
     return create_plot(stockId)
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
 if __name__ == "__main__":
     app.run(debug=True)
->>>>>>> c864b67baf93d3225fecea75a589dc97ea8de83b
